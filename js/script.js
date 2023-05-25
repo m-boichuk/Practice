b05557775da8fb4153bc222a547a2c071c6aba1b
Mykola Boichuk <nickolas.boychuk@gmail.com>
2023-05-25 22:53:29 +0300

    Coding Exercise 4

--- js/script.js
+++ js/script.js
@@ -367,3 +367,36 @@
 // for (let o = 5; o < 11; o++) {
 //   arrayOfNumbers.push(o);
 // }
+
+// Coding Exercise 4
+
+// 1
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   result[i] = arr[i];
+// }
+
+// 2
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// for (let i = 0; i < data.length; i++) {
+//   if (typeof data[i] === "number") {
+//     data[i] = data[i] * 2;
+//   } else if (typeof data[i] === "string") {
+//     data[i] = `${data[i]} - done`;
+//   }
+// }
+
+// console.log(data);
+// return data;
+
+// 3
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// const result = [];
+// for (let i = 1; i <= data.length; i++) {
+//   result[i - 1] = data[data.length - i];
+// }
+
+// console.log(result);
+// return result;
