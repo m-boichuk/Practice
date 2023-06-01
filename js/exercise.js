0f4435f46ab4ca9d4c4b71a795e599d1ee290484
Mykola Boichuk <nickolas.boychuk@gmail.com>
2023-06-01 18:27:04 +0300

    Coding Exercise 14

--- js/exercise.js
+++ js/exercise.js
@@ -313,7 +313,7 @@
 
 // availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY");
 
-// Coding Exercise 13 Робота з масивами 2
+// Coding Exercise 13
 
 // const shoppingMallData = {
 //   shops: [
@@ -360,3 +360,46 @@
 // }
 
 // isBudgetEnough(shoppingMallData);
+
+// Coding Exercise 14
+
+// const students = [
+//   "Peter",
+//   "Andrew",
+//   "Ann",
+//   "Mark",
+//   "Josh",
+//   "Sandra",
+//   "Cris",
+//   "Bernard",
+//   "Takesi",
+//   "Sam",
+// ];
+
+// function sortStudentsByGroups(arr) {
+//   arr.sort();
+//   const a = [],
+//     b = [],
+//     c = [],
+//     rest = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i < 3) {
+//       a.push(arr[i]);
+//     } else if (i < 6) {
+//       b.push(arr[i]);
+//     } else if (i < 9) {
+//       c.push(arr[i]);
+//     } else {
+//       rest.push(arr[i]);
+//     }
+//   }
+//   return [
+//     a,
+//     b,
+//     c,
+//     `Оставшиеся студенты: ${rest.length === 0 ? "-" : rest.join(", ")}`,
+//   ];
+// }
+
+// sortStudentsByGroups(students);
